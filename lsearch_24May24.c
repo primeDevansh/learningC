fa2d60f98a98e4e2be602d25d4cfed6e8f67ec1e
primeDevansh <dvcam.rastogi1980@gmail.com>
2024-05-28 11:47:33 +0530

    Add generics

--- lsearch_24May24.c
+++ lsearch_24May24.c
@@ -1,26 +1,66 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-void* lsearch(void*, void*, int, int);
+int lsearch(void *, void *, int, int);
+int my_memcmp(void *, void *, int);
 
-int main(int argc, char* argv[]) {
-    //for int array
-    int arr[] = {10, 20, 30, 40, 50};
-    int key = atoi(argv[1]);
-    int* pos = lsearch(&key, arr, (sizeof(arr) / sizeof(int)), sizeof(int));
-
-    printf("Element %d found at position %d\n", key, *pos);
-
+int main() {
+    int arr[] = {5, 4, 3, 2, 1};
+    int key = 7;
+    int pos = lsearch(&key, arr, sizeof(arr) / sizeof(int), sizeof(int));
+    printf("Element found at position: %d\n\n", pos);
     return 0;
 }
 
-void* lsearch(void* key, void* base, int n, int elemSize) {
+int lsearch(void *key, void *base, int n, int elemSize) {
     for(int i = 0; i < n; i++) {
-        void* elemAddr = (char*)base + i * elemSize;
-        if(memcmp(key, elemAddr, elemSize) == 0) {
-            return elemAddr;
-        }
+        void *elemAddr = (char*)base + i * elemSize;
+        if(my_memcmp(key, elemAddr, elemSize) == 0)
+            return i + 1;
     }
-    return NULL;
+    return -1;
 }
+
+int my_memcmp(void *elem1, void *elem2, int bytes) {
+    char *e1 = (char *)elem1;
+    char *e2 = (char *)elem2;
+
+    while(bytes--) {
+        if(*e1++ != *e2++)
+            return -1;
+    }
+    return 0;
+}
+
+
+//VERSION 1
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+
+// void* lsearch(void*, void*, int, int);
+
+// int main(int argc, char* argv[]) {
+//     //for int array
+//     int arr[] = {10, 20, 30, 40, 50};
+//     int key = atoi(argv[1]);
+//     int* elemAddr = lsearch(&key, arr, (sizeof(arr) / sizeof(int)), sizeof(int));
+
+//     //calculate position
+//     // int pos = (arr - elemAddr) / sizeof(int);
+
+//     // printf("%d %d \n", &arr[0], &arr[4]);
+
+//     printf("Element %d found at position %d\n", key, 1);
+
+//     return 0;
+// }
+
+// void* lsearch(void* key, void* base, int n, int elemSize) {
+//     for(int i = 0; i < n; i++) {
+//         void* elemAddr = (char*)base + i * elemSize;
+//         if(memcmp(key, elemAddr, elemSize) == 0) {
+//             return elemAddr;
+//         }
+//     }
+//     return NULL;
+// }
--- swap_24May24.c
+++ swap_24May24.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
-// #include <string.h>
+#include <string.h>
 
 void swap(void*, void*, long);
 void my_memcpy(void*, const void*, unsigned long);
 
 int main() {
+    /*
     // int x = 5, y = 7;
     float x = 5.35, y = 7.12;
     printf("Before Swap: x = %f, y = %f\n", x, y);
     // printf("\n%d\n", (&x)[-1]);
     swap(&x, &y, sizeof(int));
     printf("After Swap: x = %f, y = %f\n", x, y);
+    return 0;
+    */
+
+    char *ch1 = strdup("abcd"), *ch2 = strdup("efgh");
+    printf("Before Swap: ch1 = %s, y = %s\n", ch1, ch2);
+    swap(&ch1, &ch2, sizeof(char));
+    printf("After Swap: ch1 = %s, y = %s\n", ch1, ch2);
     return 0;
 }
 
@@ -24,7 +32,13 @@ void swap(void* vp1, void* vp2, long size) {
 void my_memcpy(void* dest, const void* src, unsigned long bytes) {
     char* t1 = (char*)dest;
     char* t2 = (char*)src;
-    for(int i = 0; i < bytes; i++) {
-        *(t1 + i) = t2[i];
+    // for(int i = 0; i < bytes; i++) {
+    //     *(t1 + i) = t2[i];
+    // }
+
+    //OR
+
+    while(bytes--) {
+        *t1++ = *t2++;  //ref: pg115 Dennis Ritchie book
     }
 }
