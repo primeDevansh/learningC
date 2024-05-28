5df179080db369e1e87d8875b69131cc83ed2097
primeDevansh <dvcam.rastogi1980@gmail.com>
2024-05-28 18:29:21 +0530

    Add more generic lsearch

---
+++ contrivedExp.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    char *str = "Devansh";
+    printf("%c\n", *(str + 1));
+    printf("%s\n", str);
+    return 0;
+}
--- lsearch_24May24.c
+++ lsearch_24May24.c
@@ -1,39 +1,90 @@
 #include <stdio.h>
+#include <string.h>
 
-int lsearch(void *, void *, int, int);
-int my_memcmp(void *, void *, int);
+void *lsearch(void *, void *, int, int, int (*)(void *, void *));
+int StrCmp(void *, void *);
+int IntCmp(void *, void *);
 
 int main() {
+    //for integer array LS
     int arr[] = {5, 4, 3, 2, 1};
-    int key = 2;
-    int pos = lsearch(&key, arr, sizeof(arr) / sizeof(int), sizeof(int));
-    printf("Element found at position: %d\n\n", pos);
+    int key = 5;
+
+    int *elemAddr = lsearch(&key, arr, sizeof(arr) / sizeof(int), sizeof(int), IntCmp);
+
+    printf("Element at address %p found!", elemAddr);
+
     return 0;
+    //for character array linear search
+    // char *notes[] = {"Ab", "F#", "B", "Gb", "D"};
+    // char *key = "Gb";
+
+    // char *elemAddr = lsearch(key, notes, sizeof(notes) / sizeof(char *), sizeof(char *), StrCmp);
+
+    // printf("Element %s found!", elemAddr);
+    // return 0;
 }
 
-int lsearch(void *key, void *base, int n, int elemSize) {
+void *lsearch(void *key, void *base, int n, int elemSize, int (*cmpfn)(void *, void *)) {
     for(int i = 0; i < n; i++) {
-        void *elemAddr = (char*)base + i * elemSize;
-        if(my_memcmp(key, elemAddr, elemSize) == 0)
-            return i + 1;
+        void *elemAddr = (char *)base + i * elemSize;
+        if(cmpfn(key, elemAddr) == 0)
+            return elemAddr;
     }
-    return -1;
+    return NULL;
 }
 
-int my_memcmp(void *elem1, void *elem2, int bytes) {
-    // char *e1 = (char *)elem1;
-    // char *e2 = (char *)elem2;
+int StrCmp(void *elem1, void *elem2) {
+    char *str1 = elem1;
+    char *str2 = elem2;
 
-    //what about removing the cast to char* !? Lecture 5, Su Notes Pg 3
-    char *e1 = elem1;
-    char *e2 = elem2;
-
-    while(bytes--) {
-        if(*e1++ != *e2++)
-            return -1;
-    }
-    return 0;
+    return strcmp(str1, *str2);
 }
+
+int IntCmp(void *vp1, void *vp2) {
+    int *ip1 = vp1;
+    int *ip2 = vp2;
+    
+    return *ip1 - *ip2;
+}
+
+//VERSION 2
+// #include <stdio.h>
+
+// int lsearch(void *, void *, int, int);
+// int my_memcmp(void *, void *, int);
+
+// int main() {
+//     int arr[] = {5, 4, 3, 2, 1};
+//     int key = 2;
+//     int pos = lsearch(&key, arr, sizeof(arr) / sizeof(int), sizeof(int));
+//     printf("Element found at position: %d\n\n", pos);
+//     return 0;
+// }
+
+// int lsearch(void *key, void *base, int n, int elemSize) {
+//     for(int i = 0; i < n; i++) {
+//         void *elemAddr = (char*)base + i * elemSize;
+//         if(my_memcmp(key, elemAddr, elemSize) == 0)
+//             return i + 1;
+//     }
+//     return -1;
+// }
+
+// int my_memcmp(void *elem1, void *elem2, int bytes) {
+//     // char *e1 = (char *)elem1;
+//     // char *e2 = (char *)elem2;
+
+//     //what about removing the cast to char* !? Lecture 5 - SUCS107, My Notes Pg 3
+//     char *e1 = elem1;
+//     char *e2 = elem2;
+
+//     while(bytes--) {
+//         if(*e1++ != *e2++)
+//             return -1;
+//     }
+//     return 0;
+// }
 
 
 //VERSION 1
